59968d1240156a733365dec781fd93818bf4d4d4
VUTUYEN <vutuyenkt2000@gmail.com>
2021-12-04 09:31:10 +0700

    Add avatar on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :age, presence: true
+  validates :age, numericality: { only_integer: true }
+  has_one_attached :avatar
 end
